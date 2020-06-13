93fd637a3e36a64925274f041d8653e0791e03d1
koki inoue <ko.18.5in@gmail.com>
2020-06-13 12:56:14 +0900

    add config.ganerator
    rails g コマンドで生成されるファイルの設定を追加

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,20 @@ Bundler.require(*Rails.groups)
 
 module Kokinoue
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false,
+                       fixtures: true
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
