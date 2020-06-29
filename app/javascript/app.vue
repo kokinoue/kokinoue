f5a8d78826c7dbffe38197eb826014dfa7a949b6
koki inoue <ko.18.5in@gmail.com>
2020-06-30 01:02:32 +0900

    add icon

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <img class="icon" src="./packs/assets/icon.jpg" alt="アイコン">
     <p>testです</p>
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
@@ -32,5 +33,10 @@ export default {
 p {
   font-size: 2em;
   text-align: center;
+}
+
+.icon {
+  width: 10vw;
+  height: 10vw;
 }
 </style>
