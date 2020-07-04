68b5c65f164520d3f0220b01b3f0ca5dfbf30117
koki inoue <ko.18.5in@gmail.com>
2020-07-04 15:08:58 +0900

    add Header

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,42 +1,22 @@
 <template>
   <div id="app">
-    <img class="icon" src="./packs/assets/icon.jpg" alt="アイコン">
-    <p>testです</p>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-    <router-link to="/contact">Contact</router-link>
-    <router-view />
+    <Header></Header>
   </div>
 </template>
 
 <script>
-// export default {
-//   data: function () {
-//     return {
-//       message: "Hello Vue!"
-//     }
-//   }
-// }
+import Header from './packs/components/header';
 
 export default {
   name: 'MyApp',
   props: {
     msg: String
+  },
+  components: {
+    Header
   }
-  // components: {
-  //   Header,Footer
-  // }
 }
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
-
-.icon {
-  width: 10vw;
-  height: 10vw;
-}
 </style>
---
+++ app/javascript/packs/components/header.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <header>
+    <img class="icon" src="../assets/icon.png" alt="アイコン">
+    <div class="links">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/contact">Contact</router-link>
+      <router-view />
+    </div>
+  </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+  header {
+    margin-top: 30vh;
+    text-align: center;
+  }
+
+  .icon {
+    border: 3px solid #ccc;
+    border-radius: 50%;
+    height: 10vw;
+    width: 10vw;
+  }
+</style>
