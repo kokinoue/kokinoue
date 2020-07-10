2ea42211b542c77c614691880175756414d82d82
koki inoue <ko.18.5in@gmail.com>
2020-07-11 02:07:12 +0900

    add github icon
    GitHubのアイコン用意

--- app/javascript/packs/components/header.vue
+++ app/javascript/packs/components/header.vue
@@ -5,8 +5,9 @@
       <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
       <router-link to="/contact">Contact</router-link>
-      <router-view />
+      <a href="https://github.com/kokinoue/" target="_blank"><i class="fab fa-github"></i></a>
     </div>
+    <router-view />
   </header>
   </div>
 </template>
@@ -17,16 +18,22 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
   header {
     margin-top: 30vh;
     text-align: center;
-  }
 
-  .icon {
-    border: 3px solid #ccc;
-    border-radius: 50%;
-    height: 10vw;
-    width: 10vw;
+    .icon {
+      border: 3px solid #ccc;
+      border-radius: 50%;
+      height: 10vw;
+      width: 10vw;
+    }
+
+    .links {
+      a {
+        color: #333;
+      }
+    }
   }
 </style>
