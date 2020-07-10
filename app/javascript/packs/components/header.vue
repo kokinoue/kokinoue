2a02fe0f5a29dd2186e5894747db4a7f3dc7a87d
koki inoue <ko.18.5in@gmail.com>
2020-07-11 02:26:19 +0900

    add qiita

--- app/javascript/packs/components/header.vue
+++ app/javascript/packs/components/header.vue
@@ -5,9 +5,18 @@
       <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
       <router-link to="/contact">Contact</router-link>
-      <a href="https://github.com/kokinoue/" target="_blank"><i class="fab fa-github"></i></a>
-      <a href="https://www.facebook.com/koki.inoue.921" target="_blank"><i class="fab fa-facebook"></i></a>
-      <a href="https://twitter.com/fashionenjineer" target="_blank"><i class="fab fa-twitter"></i></a>
+      <a class="links-icon" href="https://github.com/kokinoue/" target="_blank">
+        <i class="fab fa-github"></i>
+      </a>
+      <a class="links-icon" href="https://www.facebook.com/koki.inoue.921" target="_blank">
+        <i class="fab fa-facebook"></i>
+      </a>
+      <a class="links-icon" href="https://twitter.com/fashionenjineer" target="_blank">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a class="links-icon" href="https://qiita.com/ko_ki" target="_blank">
+        <img class="links-image" src="../assets/qiita.ico" alt="qiita">
+      </a>
     </div>
     <router-view />
   </header>
@@ -35,6 +44,11 @@ export default {
     .links {
       a {
         color: #333;
+      }
+
+      &-icon, &-image {
+        width: 16px;
+        height: 16px;
       }
     }
   }
