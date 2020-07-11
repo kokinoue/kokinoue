9628db7ed6554f92cd0c5b9509f22e7531931269
koki inoue <ko.18.5in@gmail.com>
2020-07-11 14:04:29 +0900

    fix header design
    margin等を整えた

--- app/javascript/packs/components/header.vue
+++ app/javascript/packs/components/header.vue
@@ -1,25 +1,29 @@
 <template lang="html">
   <header>
     <img class="icon" src="../assets/icon.png" alt="アイコン">
-    <div class="links">
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/contact">Contact</router-link>
-      <a class="links-icon" href="https://github.com/kokinoue/" target="_blank">
-        <i class="fab fa-github"></i>
-      </a>
-      <a class="links-icon" href="https://www.facebook.com/koki.inoue.921" target="_blank">
-        <i class="fab fa-facebook"></i>
-      </a>
-      <a class="links-icon" href="https://twitter.com/fashionenjineer" target="_blank">
-        <i class="fab fa-twitter"></i>
-      </a>
-      <a class="links-icon" href="https://qiita.com/ko_ki" target="_blank">
-        <img class="links-image" src="../assets/qiita.ico" alt="qiita">
-      </a>
-      <a class="links-icon" href="https://note.com/kkkkiiiii" target="_blank">
-        <img class="links-image" src="../assets/note.png" alt="note">
-      </a>
+    <div class="nav">
+      <div class="links">
+        <router-link to="/" class="router">Home</router-link>
+        <router-link to="/about" class="router">About</router-link>
+        <router-link to="/contact" class="router">Contact</router-link>
+      </div>
+      <div class="links">
+        <a class="links-icon" href="https://github.com/kokinoue/" target="_blank">
+          <i class="fab fa-github links-image"></i>
+        </a>
+        <a class="links-icon" href="https://www.facebook.com/koki.inoue.921" target="_blank">
+          <i class="fab fa-facebook links-image"></i>
+        </a>
+        <a class="links-icon" href="https://twitter.com/fashionenjineer" target="_blank">
+          <i class="fab fa-twitter links-image"></i>
+        </a>
+        <a class="links-icon" href="https://qiita.com/ko_ki" target="_blank">
+          <img class="links-image" src="../assets/qiita.ico" alt="qiita">
+        </a>
+        <a class="links-icon" href="https://note.com/kkkkiiiii" target="_blank">
+          <img class="links-image" src="../assets/note.png" alt="note">
+        </a>
+      </div>
     </div>
     <router-view />
   </header>
@@ -34,8 +38,9 @@ export default {
 
 <style scoped lang="scss">
   header {
-    margin-top: 30vh;
+    margin: 30vh auto 0;
     text-align: center;
+    width: 60vw;
 
     .icon {
       border: 3px solid #ccc;
@@ -44,14 +49,35 @@ export default {
       width: 10vw;
     }
 
-    .links {
+    .nav {
+      display: flex;
+      justify-content: space-around;
+
       a {
         color: #333;
+        text-decoration: none;
       }
 
-      &-icon, &-image {
-        width: 16px;
-        height: 16px;
+      .links {
+        .router {
+          font-size: 12px;
+          letter-spacing: 0.15em;
+          padding: 0 12px;
+
+          &-link-exact-active {
+            font-weight: bold;
+          }
+        }
+
+        &-icon {
+          display: inline-flex;
+          margin: 0 4px;
+        }
+
+        &-image {
+          width: 24px;
+          height: 24px;
+        }
       }
     }
   }
