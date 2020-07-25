3bd5f3abbbb0e3806b24f841e5eed5136b17cd35
koki inoue <ko.18.5in@gmail.com>
2020-07-25 20:27:01 +0900

    update environment
    並び替えと本番対応

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,16 @@
 Rails.application.configure do
-  config.hosts << "agile-mountain-76200.herokuapp.com"
+  config.assets.debug = true
+  config.assets.quiet = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = false
+  config.active_storage.service = :local
+  config.active_support.deprecation = :log
+  config.active_record.migration_error = :page_load
+  config.active_record.verbose_query_logs = true
   config.cache_classes = false
-  config.eager_load = false
   config.consider_all_requests_local = true
+  config.eager_load = false
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
@@ -15,14 +23,4 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
     config.cache_store = :null_store
   end
-
-  config.active_storage.service = :local
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_caching = false
-  config.active_support.deprecation = :log
-  config.active_record.migration_error = :page_load
-  config.active_record.verbose_query_logs = true
-  config.assets.debug = true
-  config.assets.quiet = true
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,21 +1,23 @@
 Rails.application.configure do
-  config.hosts << "agile-mountain-76200.herokuapp.com"
-  config.cache_classes = true
-  config.eager_load = true
-  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-  # config.require_master_key = true
-  config.public_file_server.enabled = true
-  config.assets.compile = false
+  config.action_mailer.perform_caching = false
+  config.active_record.dump_schema_after_migration = false
   config.active_storage.service = :local
+  config.active_support.deprecation = :notify
+  config.assets.compile = false
+  config.cache_classes = true
+  config.consider_all_requests_local = false
+  config.eager_load = true
+  config.force_ssl = true
+  config.hosts << "agile-mountain-76200.herokuapp.com"
+  config.hosts << "kokinoue.com"
+  config.i18n.fallbacks = true
+  config.log_formatter = ::Logger::Formatter.new
   config.log_level = :debug
   config.log_tags = [:request_id]
-  config.action_mailer.perform_caching = false
-  config.i18n.fallbacks = true
-  config.active_support.deprecation = :notify
-  config.log_formatter = ::Logger::Formatter.new
-  config.active_record.dump_schema_after_migration = false
+  config.public_file_server.enabled = true
   config.read_encrypted_secrets = true
+  config.require_master_key = true
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
