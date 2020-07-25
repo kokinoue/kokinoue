83b7ae16a00473b6dc83640b141459973c8c5612
koki inoue <ko.18.5in@gmail.com>
2020-07-25 20:27:36 +0900

    changes for production

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,9 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
+
+preload_app!
+
 port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
