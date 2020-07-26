b339f6f4a790e3e347703591099d0a5b982b6e7c
koki inoue <ko.18.5in@gmail.com>
2020-07-26 12:06:24 +0900

    change about view

--- app/javascript/packs/pages/about.vue
+++ app/javascript/packs/pages/about.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="about">
-    <p>aboutのpage</p>
+    <table class="about-table">
+      <tr class="about-table-row">
+        <th class="about-table-row-title">Name: </th>
+        <td class="about-table-row-value">Koki Inoue / 井上航輝</td>
+      </tr>
+      <tr class="about-table-row">
+        <th class="about-table-row-title">Job: </th>
+        <td class="about-table-row-value">Software Engineer</td>
+      </tr>
+      <tr class="about-table-row">
+        <th class="about-table-row-title">Company: </th>
+        <td class="about-table-row-value">READY TO FASHION. Inc</td>
+      </tr>
+      <tr class="about-table-row">
+        <th class="about-table-row-title">Hobby: </th>
+        <td class="about-table-row-value">Fashion, Photo, Ski, Trip</td>
+      </tr>
+    </table>
   </div>
 </template>
 <script>
@@ -13,5 +30,22 @@
       @media all and (max-width: $size) {
           @content;
       }
+  }
+
+  .about {
+    font-size: 20px;
+    margin: 20px 0;
+    @include query($break-small) {
+      font-size: 16px;
+    }
+
+    &-table {
+      margin: 0 auto;
+
+      &-row {
+        display: block;
+        margin: 16px 0;
+      }
+    }
   }
 </style>
