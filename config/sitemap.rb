f0a916e0db30fe20e1950bee05b91cf05c85711a
koki inoue <ko.18.5in@gmail.com>
2020-07-31 18:21:25 +0900

    add GCS to sitemap
    jsonは秘密ファイルから読み込むように変更する必要がある

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,4 +1,16 @@
+require 'google/cloud/storage'
+
 SitemapGenerator::Sitemap.default_host = 'https://kokinoue.com'
+
+SitemapGenerator::Sitemap.sitemaps_host = 'https://console.cloud.google.com/storage/browser/kokinoue'
+
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::GoogleStorageAdapter.new(
+  credentials: 'config/gcs.json',
+  project_id: 'kokinoue',
+  bucket: 'kokinoue'
+)
 
 SitemapGenerator::Sitemap.create do
   add '/about', changefreq: 'weekly'
